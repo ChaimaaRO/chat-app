ec0cd8d3cb9b182451adb15650440f9c3e596e29
Houda SAMAD <houda.samad@um5.ac.ma>
2023-04-24 23:22:21 +0100

    setting up the firebase

---
+++ app/src/main/java/com/example/mychat/activities/SignInActivity.java
@@ -0,0 +1,28 @@
+package com.example.mychat.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mychat.databinding.ActivitySignInBinding;
+
+public class SignInActivity extends AppCompatActivity {
+    private ActivitySignInBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListener();
+    }
+
+    private void setListener() {
+        binding.textCreateNewAccount.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+
+    }
+
+
+
+}
